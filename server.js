81c8da2e1cd196fd65ffc5b583477f34e7ca75f2
Olive Daly <olive-branch@posteo.me>
2022-12-27 15:17:39 -0500

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const gridGen = require("./utils");
+const cors = require("cors");
 console.log(gridGen)
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
